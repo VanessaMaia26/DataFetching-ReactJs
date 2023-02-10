f59ad1d72709c5f6d6458b807989eec7983e732d
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-02-10 11:22:33 -0300

    First commit

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,78 @@
+import { Component } from 'react';
+
+import './styles.css';
+
+import { Posts } from '../../components/Posts';
+import { loadPosts } from '../../utils/load-posts'
+import { Button } from '../../components/Button';
+
+export class Home extends Component {
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 10,
+    searchValue: '',
+  };
+
+   async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const { page, postsPerPage} = this.state;
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos
+    });
+  }
+
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+  }
+
+  handleChange = (e) => {
+    const {value} = e.target;
+    this.setState({ searchValue: value });
+  }
+
+  render() {
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    return (
+      <section className='container'>
+        
+        <h1>Search Value: {searchValue}</h1>
+
+        <input
+          onChange={this.handleChange}
+          type="search"
+          value={searchValue}
+        />
+
+        <Posts posts={posts} />
+
+        <div className='button-container'>
+          <Button
+            text="Load more posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </div>
+      </section>
+    );
+  }
+}
